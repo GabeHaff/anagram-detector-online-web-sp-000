d34b11862e6d75edc1cd3dcd4247abbf1450f49d
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-21 20:48:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,13 +14,4 @@ end
 end 
 end
   
-   #takes anagram array, return matches in a different array possibly as input, not as an argument? 
-  #  anagram_array.each do some_word.split
-  #iterate over words that method takes as argument.
-  #use split() // == to compare the two arrays to see if theyre composed of the same letters 
-  #equal if they contain the same elements, in the same order. 
-  # use .sort to sort an arrays elements, or [array].sort == [array].sort
-  
-  
-  
-  
+   
